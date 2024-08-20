bf2ec5c787407b9eb16c0300b021baf555946b9c
BishalSunuwar202 <bishalsunuwar24@gmail.com>
2024-08-20 11:57:50 +0545

    footer section

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+const Footer = () => {
+  return (
+    <div className="text-white flex justify-between text-sm mt-16 p-2 font-thin items-center">
+      <p className="text-xs ml-2">© 2011-2021 THE SMS CENTRAL</p>
+      <div className="flex flex-col items-center mr-6">
+        <h1 className="font-bold">THE SMS CENTRAL</h1>
+        <p>Baneshwor, Kathmandu, Nepal</p>
+      </div>
+      <div className="mr-2">Terms of Use</div>
+    </div>
+  );
+};
+
+export default Footer;
